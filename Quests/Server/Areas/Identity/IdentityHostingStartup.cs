ca3cfba0c206d0863fb49cda33cefaa38e6ab59c
hellloy <hellloy@gmail.com>
2020-08-21 19:30:29 +0300

    Добавьте файлы проекта.

---
+++ Quests/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Quests.Server.Data;
+using Quests.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Quests.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Quests.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
