abf8518edb8b3608cf5e34ba6b04a348f5d9d406
hellloy <hellloy@gmail.com>
2020-11-24 11:19:13 +0300

    disable send email after regiter

--- Quests/Server/Startup.cs
+++ Quests/Server/Startup.cs
@@ -62,6 +62,9 @@ namespace Quests.Server
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequireUppercase = false;
                     options.Password.RequiredLength = 6;
+                    options.SignIn.RequireConfirmedEmail = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
 
                 })
                 .AddRoles<IdentityRole>()
